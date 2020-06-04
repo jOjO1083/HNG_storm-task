5e9e97ecece15b577e00e1f8158cd41bd21a92e2
jOjO1083 <64544736+jOjO1083@users.noreply.github.com>
2020-06-04 15:20:17 +0100

    Create jOjO.php

---
+++ testScripts/jOjO.php
@@ -0,0 +1,14 @@
+<?php
+
+$fullName = "Jonathan Joseph Ikpe";
+$id = "HNG-05569";
+$email = "jonathan.joseph43@yahoo.com";
+$language = "PHP";
+
+
+echo "
+ Hello World, this is " . $fullName . " with HNGi7 ID " 
+ . $id . " and email " . $email . " using " 
+ . $language . " for stage 2 task";
+ 
+?>
